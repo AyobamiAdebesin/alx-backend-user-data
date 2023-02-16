622869b383ca268c9b42f599ccdb28da80bf0e99
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-16 18:07:05 +0100

    Refactor: modified class import

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Mode for a user class """
-from sqlalchemy import Column, String, Integer, String, Metadata
+from sqlalchemy import Column, String, Integer, String, MetaData
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-my_metadata = Metadata()
+my_metadata = MetaData()
 Base = declarative_base(metadata=my_metadata)
 
 
@@ -12,7 +12,7 @@ class User(Base):
     """ User class that represents the user table """
     __tablename__ = 'users'
     id = Column(Integer(), unique=True, nullable=False, primary_key=True)
-    email = Column(String(255), nullable=False)
-    hashed_password = Column(String(256), nullable=False)
-    session_id = Column(String(256), nullable=True)
-    reset_token = Column(String(256), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
