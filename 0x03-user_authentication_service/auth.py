aaa03c5480fce9674648a60a4ef607a8e39fcec4
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-16 21:48:13 +0100

    added documentation and fixed pycodestyle error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Encrypting a password with an hash function """
-from bcrypt import hashpw,gensalt
+from bcrypt import hashpw, gensalt
 
 
 def _hash_password(password: str) -> bytes:
     """ Hashing a password with bcrypt """
-    #Encode password before hashing
+    # Encode password before hashing
     pwd_bytes = password.encode('utf-8')
     salt = gensalt()
     return hashpw(pwd_bytes, salt)
