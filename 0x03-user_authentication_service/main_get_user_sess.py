db7eabf9904d3228b3f592e9afe48202c7f36c03
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-22 17:34:49 +0100

    Deleted test files

--- 0x03-user_authentication_service/main_get_user_sess.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-""" Main file """
-
-from auth import Auth
-
-auth = Auth()
-
-print(auth.get_user_from_session(session_id="6fd008b8-40bb-4cb8-b69d-796e107ac6c7"))
--- 0x03-user_authentication_service/main_uuid.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" Main file """
-from auth import Auth
-
-auth = Auth()
-
-print(auth._generate_uuid())
