a8138934192e86559ee41e7d7424a21be3815ad3
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-22 14:50:24 +0100

    Implementation of user profile's page

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,5 +56,18 @@ def logout_user():
         abort(403)
 
 
+@app.route("/profile", methods=['GET'])
+def get_profile():
+    """ Route to the profile page """
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
