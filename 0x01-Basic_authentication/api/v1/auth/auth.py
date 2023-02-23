de67755d43236125ea7219681a8cb39a5e174e7d
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-23 17:28:45 +0100

    Implementation of requests that do not require authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,26 @@ from typing import List, TypeVar
 class Auth:
     """ Authentication class for the API """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Requires authentication """
-        return False
+        """
+        Defines which routes don't need authentication
+
+        excluded_paths contains string path always ending by a "/"
+        """
+        slash_tolerant = "{}/"
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path:
+            if path.endswith("/"):
+                if path in excluded_paths:
+                    return False
+            else:
+                path = path+slash_tolerant
+                if path in excluded_paths:
+                    return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
