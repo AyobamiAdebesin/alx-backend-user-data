e0215f26a8d0d51b8c4fae0a317214c2a216c387
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-25 08:20:33 +0100

    Searching for User object with extracted credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Authentication """
 import base64
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -45,3 +47,20 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeError('User'):
+        """ Create user object from credentials passed extracted from header """
+        if user_email is None or type(user_email) or type(user_email) != str:
+            return
+        if user_pwd is None or type(user_pwd) != str:
+            return
+        if User.count() == 0:
+            return
+        if len(User.search({"email": user_email})) == 0:
+            return None
+        else:
+            user = User.search({"email": user_email})[0]
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return
