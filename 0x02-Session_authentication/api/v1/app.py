79b126b8370efd5fc658be1cd8654c9f86662583
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-26 22:58:11 +0100

    Trying to fix checker error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,7 +61,7 @@ def filter_request() -> Any:
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
-            'api/v1/auth_session/login/'
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
             pass
