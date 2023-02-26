4138e9daa9a9b476e07664e7a631907d88e83639
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-26 21:07:26 +0100

    Created a nethod for creating session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 """ A Session Authentication class """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ A session authentication model """
+    user_id_by_session = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session ID given a user_id
+
+        This session id is stored as a key in the user_id_by_session
+        dict with the user_id as a value. This is because a user_id can have
+        multiple session id
+        """
+        if type(user_id) == str:
+            sess_id = str(uuid4())
+            user_id_by_session[sess_id] = user_id
+            return sess_id
+        else:
+            return
