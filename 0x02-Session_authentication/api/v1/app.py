f9958c65f336ddce09fff60433728ddfd80d6058
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-26 22:44:12 +0100

    Fixed check condition

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -65,8 +65,8 @@ def filter_request() -> Any:
         ]
         if auth.require_auth(request.path, excluded_paths):
             pass
-            if auth.authorization_header(request) or None \
-                    and auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None \
+                    or auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
